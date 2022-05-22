3e72e66344bd8f29deaa21c5c9030111c971a784
Kasra Oskouei <kasra.oskouei@dormakaba-external.com>
2022-05-22 15:29:42 +0300

    groups models implemented

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,44 @@
+from audioop import reverse
 from django.db import models
-
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+from django import template
+import misaka
 # Create your models here.
+
+User = get_user_model()
+register = template.Library()
+# https://docs.djangoproject.com/en/2.0/howto/custom-template-tags/#inclusion-tags
+# This is for the in_group_members check template tag
+
+class Group(models.Model):
+    name = models.CharField(max_length=256, unique = True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default="")
+    description_html = models.TextField(editable=False, default="", blank=True)
+    members = models.ManyToManyField(User, through="GroupMember")
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs = {"slug":self.slug})
+    
+    class Meta:
+        ordering = ["name"]
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name="memberships", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="user_groups", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ("group", "user")
+
